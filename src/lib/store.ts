e2bbab674637ccca155116c9860cd6cad0bb18b5
Omarnnaar <omar.m.m.nasrallah@gmail.com>
2026-02-08 16:18:43 +0300

    fixes store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -9,7 +9,7 @@ import {
 } from './data';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://34.198.205.91:8080';
-const API_BASE_URL_ROUTING =  process.env.NEXT_PUBLIC_API_URL || 'https://master.d2hh6o27ll1srl.amplifyapp.com/';
+const API_BASE_URL_ROUTING =  https://newbranch.d3j08vx9mueaso.amplifyapp.com/ ;
 // const API_BASE_URL_ROUTING = 'http://localhost:3001/';
 
 interface AppState {
